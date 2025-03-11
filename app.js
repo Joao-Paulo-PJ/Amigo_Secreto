b037a9b7556515750658e41cae4391d399c7bdc1
João Paulo da Silva <joaopaulo.jpds15@gmail.com>
2025-03-11 19:52:37 -0300

    Passo final

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+   let adicionando = document.getElementById("amigo");
+   let nome = adicionando.value.trim(); // Captura e remove espaços extras
+   if (nome === "") {
+    alert("Por favor, insira um nome.");
+    return;
+   }
+   amigos.push(nome); // Adiciona ao array
+   atualizarLista();
+   adicionando.value = ""; // Limpa o campo após adicionar
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpa a lista antes de atualizar
+
+    amigos.forEach((amigo, index) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um amigo antes de sortear!");
+        return;
+    }
+
+    //Gera o índice sorteado aleatoriamente
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceSorteado];
+
+    // Esconde a lista de amigos e exibe o sorteio
+    document.getElementById("listaAmigos").style.display = "none";
+    document.getElementById("resultado").innerHTML = `O amigo secreto sorteado é: ${amigoSorteado}`;
+}
